406d58f10202bbaa0f0012942d4383f5006b70f1
CESAR1102 <cesar02_11@hotmail.com>
2019-10-26 00:22:12 -0500

    agregar Booking/Booking message/detail Parking

--- driverfeature/src/main/java/com/example/driverfeature/BookingActivity.kt
+++ driverfeature/src/main/java/com/example/driverfeature/BookingActivity.kt
@@ -2,11 +2,22 @@ package com.example.driverfeature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.content.Intent
+
 
 class BookingActivity : AppCompatActivity() {
+    private val LOG_TAG = BookingActivity::class.java!!.getSimpleName()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking)
+    }
+
+    fun reserveSecondActivity(view: View) {
+        Log.d(LOG_TAG, "Button clicked!");
+        val intent = Intent(this, BookingSecondActivity::class.java)
+        startActivity(intent);
     }
 }
