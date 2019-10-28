b88b70cd85edeccb70c71cffc0d0ee1ee28446df
Oscar Rojas <oscarrda78@gmail.com>
2019-10-28 01:27:46 -0500

    Repository changes

---
+++ AppAndroid/app/src/main/java/com/example/myapplication/ui/home/MapFragment.kt
@@ -0,0 +1,69 @@
+package com.example.myapplication.ui.home
+
+import android.location.Location
+import android.location.LocationListener
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.myapplication.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapFragment : Fragment(), OnMapReadyCallback, LocationListener {
+    private  var googleMap: GoogleMap?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment=childFragmentManager.findFragmentById(com.example.driverfeature.R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(com.example.driverfeature.R.layout.fragment_google_map, container, false)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+
+        this.googleMap=p0
+        val point= LatLng(13.03,77.60)
+        googleMap!!.addMarker(MarkerOptions().position(point).title("Prueba").snippet("1"))
+        googleMap!!.animateCamera(CameraUpdateFactory.newLatLng(point))
+        googleMap!!.setOnMarkerClickListener { Toast.makeText(this.activity,"Nombre"+it.title+" Location: "+it.position,
+            Toast.LENGTH_LONG).show(); true ;}
+    }
+
+
+
+    override fun onLocationChanged(location: Location?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    // TODO: Rename and change types of parameters
+}
---
+++ AppAndroid/driverfeature/src/main/java/com/example/driverfeature/BookingSecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.driverfeature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookingSecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking_second)
+    }
+}
