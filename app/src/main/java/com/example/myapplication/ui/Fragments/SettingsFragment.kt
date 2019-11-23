9e9b546ed3d0dc3ee4d577636a3486e8cb10b96c
estefano zegarra <estefano.zegarra@aex-it.com>
2019-11-23 01:30:25 -0500

    vista settings añadida

---
+++ app/src/main/java/com/example/myapplication/ui/Fragments/SettingsFragment.kt
@@ -0,0 +1,98 @@
+package com.example.myapplication.ui.Fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.fragment_settings.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [SettingsFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [SettingsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SettingsFragment : Fragment() {
+    var MapStyle = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapSatelital.setOnClickListener {
+            changeSatelitalStyle()
+        }
+        mapNormal.setOnClickListener {
+            changeNormalStyle()
+        }
+    }
+
+    private fun changeNormalStyle(){
+        mapNormal.scaleType = ImageView.ScaleType.FIT_CENTER
+        mapSatelital.scaleType = ImageView.ScaleType.CENTER
+        MapStyle = "Normal"
+    }
+
+    private fun changeSatelitalStyle(){
+        mapSatelital.scaleType = ImageView.ScaleType.FIT_CENTER
+        mapNormal.scaleType = ImageView.ScaleType.CENTER
+        MapStyle = "Satelital"
+    }
+
+
+
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SettingsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SettingsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
